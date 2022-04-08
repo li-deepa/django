46fef15925245444b6a8d298faf0b331a104bafd
li-deepa <li.deepa02@gmail.com>
2022-04-08 11:52:50 -0700

    adding all changes to model

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,8 +1,14 @@
+from contextlib import AbstractAsyncContextManager
 from django.db import models
 from django.contrib.auth.models import User
 #from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
 class Topic(models.Model):
     name=models.CharField(max_length=200)
   
@@ -40,4 +46,5 @@ class Message(models.Model):
 
     def __str__(self):
         return self.body[0:50]
+
 
